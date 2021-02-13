b1b2bcfed8ab43673294771ebf4fb08e03dd5095
amesar <amesar@users.noreply.github.com>
2021-02-13 04:48:06 +0000

    setup.py: using find_packages now

--- setup.py
+++ setup.py
@@ -1,13 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name="mlflow_tools",
       version="1.0.0",
       author="Andre M",
       description="MLflow Tools",
       url="https://github.com/amesar/mlflow-tools",
-      packages=['mlflow_tools',
-                'mlflow_tools.common',
-                'mlflow_tools.tools',
-                'mlflow_tools.export_import'
-                ],
+      packages=find_packages(),
       zip_safe=False)
