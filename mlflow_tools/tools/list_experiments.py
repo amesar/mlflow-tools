b7a6a5848707ef7dfd29e8ba97576ff208c5044c
amesar <amesar@users.noreply.github.co>
2022-11-20 21:33:06 -0500

    added test_dumps.py

--- mlflow_tools/tools/list_experiments.py
+++ mlflow_tools/tools/list_experiments.py
@@ -11,26 +11,19 @@ client = mlflow.tracking.MlflowClient()
 print("MLflow Version:", mlflow.version.VERSION)
 print("MLflow Tracking URI:", mlflow.get_tracking_uri())
 
-@click.command()
-@click.option("--csv-file", help="Output CSV file", default="experiments.csv")
-@click.option("--sort", help="Sort by this attibute", default="name")
-@click.option("--verbose", help="Show artifacts", type=bool, default=False, show_default=False)
-def main(csv_file, sort, verbose):
-    print("Options:")
-    for k,v in locals().items(): print(f"  {k}: {v}")
-
+def list(csv_file, sort_attribute="name", verbose=False):
     exps = client.search_experiments()
     print("Found {} experiments".format(len(exps)))
 
-    if sort == "name":
+    if sort_attribute == "name":
         exps = sorted(exps, key=lambda x: x.name)
-    elif sort == "experiment_id":
+    elif sort_attribute == "experiment_id":
         exps = sorted(exps, key=lambda x: int(x.experiment_id))
 
     if verbose:
-        if sort == "lifecycle_stage":
+        if sort_attribute == "lifecycle_stage":
             exps = sorted(exps, key=lambda x: x.lifecycle_stage)
-        elif sort == "artifact_location":
+        elif sort_attribute == "artifact_location":
             exps = sorted(exps, key=lambda x: x.artifact_location)
         list = [(exp.experiment_id, exp.name, exp.lifecycle_stage, exp.artifact_location) for exp in exps ]
         df = pd.DataFrame(list,columns=["experiment_id","name","lifecycle_stage","artifact_location"])
@@ -42,6 +35,16 @@ def main(csv_file, sort, verbose):
         df.to_csv(f, index=False)
 
     print(tabulate(df, headers='keys', tablefmt='psql'))
+
+@click.command()
+@click.option("--csv-file", help="Output CSV file", default="experiments.csv", show_default=True)
+@click.option("--sort", help="Sort by this attibute", default="name", show_default=True)
+@click.option("--verbose", help="Verbose", type=bool, default=False, show_default=True)
+
+def main(csv_file, sort, verbose):
+    print("Options:")
+    for k,v in locals().items(): print(f"  {k}: {v}")
+    list(csv_file, sort, verbose)
 
 if __name__ == "__main__":
     main()
---
+++ tests/test_dumps.py
@@ -0,0 +1,24 @@
+import mlflow
+from mlflow_tools.tools.dump_run import dump_run_id
+from mlflow_tools.tools import list_experiments
+from . utils_test import create_experiment
+
+exp = create_experiment()
+with mlflow.start_run() as run:
+    mlflow.log_metric("m1", 0.1)
+
+run_id = run.info.run_id
+artifact_max_level = 4
+
+def test_dump_run_as_json():
+    dump_run_id(run_id, artifact_max_level, "json", True)
+
+def test_dump_run_as_yaml():
+    dump_run_id(run_id, artifact_max_level, "yaml", True)
+
+def test_dump_run_as_txt():
+    dump_run_id(run_id, artifact_max_level, "txt", True)
+
+def test_list_experiments():
+    path = "/tmp/experiments.csv" # TODO: make a tmpdir
+    list_experiments.list(path)
