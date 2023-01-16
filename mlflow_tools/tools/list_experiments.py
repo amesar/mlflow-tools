c71c6fa6d8bad512775c67876fbe5d88b519efa8
amesar <amesar@users.noreply.github.co>
2023-01-16 15:18:05 -0500

    list_experiments.py: refactor of sort columns to use pandas dataframe

--- mlflow_tools/tools/list_experiments.py
+++ mlflow_tools/tools/list_experiments.py
@@ -14,41 +14,39 @@ print("MLflow Version:", mlflow.version.VERSION)
 print("MLflow Tracking URI:", mlflow.get_tracking_uri())
 
 
-def list(csv_file, sort_attribute="name", sort_order="asc", verbose=False):
+def to_pandas_dataframe(sort_attribute="name", sort_order="asc", verbose=False):
     exps = [ exp for exp in ListExperimentsIterator(client) ]
     print(f"Found {len(exps)} experiments")
-
     if sort_attribute == "name":
         exps = sorted(exps, key=lambda x: x.name)
     elif sort_attribute == "experiment_id":
         exps = sorted(exps, key=lambda x: int(x.experiment_id))
-
     if verbose:
-        reverse = sort_order == "desc"
-        if sort_attribute == "lifecycle_stage":
-            exps = sorted(exps, key=lambda x: x.lifecycle_stage)
-        elif sort_attribute == "artifact_location":
-            exps = sorted(exps, key=lambda x: x.artifact_location)
-        elif sort_attribute == "creation_time":
-            exps = sorted(exps, key=lambda x: x.creation_time, reverse=reverse)
-        list = [(exp.experiment_id, exp.name, format_time(exp.creation_time), exp.lifecycle_stage, exp.artifact_location) for exp in exps ]
-        df = pd.DataFrame(list, columns=["experiment_id","name","creation_time","lifecycle_stage","artifact_location"])
+        list = [(exp.experiment_id, exp.name, 
+                 format_time(exp.creation_time), format_time(exp.last_update_time), 
+                 exp.lifecycle_stage, exp.artifact_location) 
+             for exp in exps ]
+        columns = ["experiment_id", "name", "creation_time", "last_update_time", "lifecycle_stage", "artifact_location"]
     else:
-        list = [(str(exp.experiment_id), exp.name) for exp in exps ]
-        df = pd.DataFrame(list,columns=["experiment_id","name"])
+        list = [(exp.experiment_id, exp.name) for exp in exps ]
+        columns = ["experiment_id", "name"]
+    df = pd.DataFrame(list, columns=columns)
+    if sort_attribute in df.columns:
+        df.sort_values(by=[sort_attribute], inplace=True, ascending=sort_order == "asc")
+    return df
 
+
+def list(csv_file, sort_attribute="name", sort_order="asc", verbose=False):
+    df = to_pandas_dataframe(sort_attribute, sort_order, verbose)
     with open(csv_file, "w", encoding="utf-8") as f:
         df.to_csv(f, index=False)
-
     print(tabulate(df, headers="keys", tablefmt="psql", showindex=False))
-
-    print(f"Found {len(exps)} experiments")
 
 
 @click.command()
 @click.option("--csv-file", help="Output CSV file", default="experiments.csv", show_default=True)
 @click.option("--sort-attr", help="Sort by this attibute", default="name", show_default=True)
-@click.option("--sort-order", help="Sort by this attibute", default="name", show_default=True)
+@click.option("--sort-order", help="Sort order: asc|desc", default="asc", show_default=True)
 @click.option("--verbose", help="Verbose", type=bool, default=False, show_default=True)
 def main(csv_file, sort_attr, sort_order, verbose):
     print("Options:")
