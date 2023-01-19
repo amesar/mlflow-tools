b561410eb9612c9e9d8c0955f7924f57055407b4
Andre Mesarovic <amesar@users.noreply.github.com>
2023-01-19 13:57:35 -0500

    Issue #4: MLflow dump notebooks - new file

---
+++ databricks_notebooks/_README.py
@@ -0,0 +1,16 @@
+# Databricks notebook source
+# MAGIC %md ## MLflow Dump Tools
+# MAGIC 
+# MAGIC List
+# MAGIC * [List_Experiments]($List_Experiments)
+# MAGIC * [List_Registered_Models]($List_Registered_Models)
+# MAGIC 
+# MAGIC Dump
+# MAGIC * [Dump_Experiment]($Dump_Experiment)
+# MAGIC * [Dump_Registered_Model]($Dump_Registered_Model)
+# MAGIC * [Dump_Run]($Dump_Run)
+# MAGIC 
+# MAGIC Public documentation
+# MAGIC * [MLflow Spark Datasource Example](https://docs.databricks.com/_static/notebooks/mlflow/mlflow-datasource.html)
+# MAGIC 
+# MAGIC Last updated: 2023-01-19
