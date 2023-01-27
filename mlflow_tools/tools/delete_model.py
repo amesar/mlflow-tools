57694f91237b7c0f43241004af84ba4191731317
amesar <amesar@users.noreply.github.co>
2023-01-27 07:05:01 -0500

    refactored and cleaned up delete_model.py and mlflow_utils.py

--- mlflow_tools/tools/delete_model.py
+++ mlflow_tools/tools/delete_model.py
@@ -4,10 +4,22 @@ Delete a registered model.
 
 import click
 import mlflow
-from mlflow_tools.common import model_utils
 
-client = mlflow.client.MlflowClient()
 print("MLflow Tracking URI:", mlflow.get_tracking_uri())
+
+
+def delete_model(model_name, client=None):
+    """ Delete a model and all its versions. """
+    if not client:
+        mlflow.client.MlflowClient()
+    versions = client.search_model_versions(f"name='{model_name}'") # TODO: handle page token
+    print(f"Deleting {len(versions)} versions for model '{model_name}'")
+    for vr in versions:
+        print(f"Deleting version={vr.version} stage={vr.current_stage} status={vr.status} run_id={vr.run_id}")
+        if vr.current_stage != "Archived": # NOTE: for Databricks though OSS works
+            client.transition_model_version_stage (model_name, vr.version, "Archived")
+        client.delete_model_version(model_name, vr.version)
+    client.delete_registered_model(model_name)
 
 
 @click.command()
@@ -15,7 +27,7 @@ print("MLflow Tracking URI:", mlflow.get_tracking_uri())
 def main(model):
     print("Options:")
     for k,v in locals().items(): print(f"  {k}: {v}")
-    model_utils.delete_model(client, model)
+    delete_model(model)
 
 
 if __name__ == "__main__":
