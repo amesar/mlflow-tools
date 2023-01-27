cb77ce1298f3b1e20f770ff5949f53754d9f989e
amesar <amesar@users.noreply.github.co>
2023-01-27 02:39:59 -0500

    model_utils.py: fixed delete_model() so it deletes all model versions with search_model_versions

--- mlflow_tools/common/model_utils.py
+++ mlflow_tools/common/model_utils.py
@@ -2,28 +2,28 @@ import time
 from mlflow.exceptions import RestException
 from mlflow.entities.model_registry.model_version_status import ModelVersionStatus
 
+
 def delete_model(client, model_name):
     """ Delete a model and all its versions. """
-    versions = client.get_latest_versions(model_name)
-    print(f"Deleting {len(versions)} latest versions for model '{model_name}'")
-    for v in versions:
-        print(f"  latest version={v.version} status={v.status} stage={v.current_stage} run_id={v.run_id}")
-        client.transition_model_version_stage (model_name, v.version, "Archived")
-        client.delete_model_version(model_name, v.version)
+    versions = client.search_model_versions(f"name='{model_name}'") # TODO: handle page token
+    print(f"Deleting {len(versions)} versions for model '{model_name}'")
+    for vr in versions:
+        print(f"Deleting version={vr.version} stage={vr.current_stage} status={vr.status} run_id={vr.run_id}")
+        if vr.current_stage != "Archived": # NOTE: for Databricks though OSS works
+            client.transition_model_version_stage (model_name, vr.version, "Archived")
+        client.delete_model_version(model_name, vr.version)
     client.delete_registered_model(model_name)
+
 
 def wait_until_version_is_ready(client, model_name, model_version, sleep_time=1, iterations=100):
     """ For Databricks. Due to blob eventual consistency, wait until a newly created version is in READY state. """
     start = time.time()
     for _ in range(iterations):
-        version = client.get_model_version(model_name, model_version.version)
-        status = ModelVersionStatus.from_string(version.status)
-        _show_version(version)
+        vr = client.get_model_version(model_name, model_version.version)
+        status = ModelVersionStatus.from_string(vr.status)
+        print(f"Version: id={vr.version} status={vr.status} state={vr.current_stage}")
         if status == ModelVersionStatus.READY:
             break
         time.sleep(sleep_time)
     end = time.time()
     print(f"Waited {round(end-start,2)} seconds")
-
-def _show_version(version):
-    print(f"Version: id={version.version} status={version.status} state={version.current_stage}")
--- mlflow_tools/tools/delete_model.py
+++ mlflow_tools/tools/delete_model.py
@@ -6,8 +6,9 @@ import click
 import mlflow
 from mlflow_tools.common import model_utils
 
-client = mlflow.tracking.MlflowClient()
+client = mlflow.client.MlflowClient()
 print("MLflow Tracking URI:", mlflow.get_tracking_uri())
+
 
 @click.command()
 @click.option("--model", help="Registered model name", required=True, type=str)
@@ -15,6 +16,7 @@ def main(model):
     print("Options:")
     for k,v in locals().items(): print(f"  {k}: {v}")
     model_utils.delete_model(client, model)
+
 
 if __name__ == "__main__":
     main()
