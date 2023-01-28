7a7f63167ab6de90747b28055b725d82ccf0fd90
Andre Mesarovic <amesar@users.noreply.github.com>
2023-01-28 00:42:52 -0500

    Delete_Registered_Model notebook: added delete_only_versions option

--- databricks_notebooks/Delete_Registered_Model.py
+++ databricks_notebooks/Delete_Registered_Model.py
@@ -9,13 +9,19 @@
 
 dbutils.widgets.removeAll()
 
-dbutils.widgets.text("Registered model", "")
-model = dbutils.widgets.get("Registered model")
+dbutils.widgets.text("1. Registered model", "")
+model = dbutils.widgets.get("1. Registered model")
 
-print("model:",model)
-assert_widget(model, "Registered model")
+dbutils.widgets.dropdown("2. Delete only versions","no",["yes","no"])
+delete_only_versions = dbutils.widgets.get("2. Delete only versions") == "yes"
+
+print("model:", model)
+print("delete_only_versions:", delete_only_versions)
+
+assert_widget(model, "1. Registered model")
 
 # COMMAND ----------
 
 from mlflow_tools.tools.delete_model import delete_model
-delete_model(model)
+
+delete_model(model, delete_only_versions)
