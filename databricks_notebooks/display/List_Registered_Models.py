994af7d9a9a5d09029078d596cd9af5401a3f25f
Andre Mesarovic <amesar@users.noreply.github.com>
2023-03-05 21:17:34 -0500

    New "display" Databricks notebooks

---
+++ databricks_notebooks/display/List_Registered_Models.py
@@ -0,0 +1,15 @@
+# Databricks notebook source
+# MAGIC %md ## List MLflow Registered Models
+
+# COMMAND ----------
+
+# MAGIC %run ./Common
+
+# COMMAND ----------
+
+from mlflow_tools.api import api_factory
+pandas_api = api_factory.get_pandas_api()
+pdf = pandas_api.search_registered_models()
+
+df = spark.createDataFrame(pdf)
+display(df)
