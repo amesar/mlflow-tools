a0dd2ab42aa6dd159f497a4fe19e8a84f3fbfad2
Andre Mesarovic <amesar@users.noreply.github.com>
2023-03-05 21:14:42 -0500

    New "display" Databricks notebooks

---
+++ databricks_notebooks/display/_README.py
@@ -0,0 +1,17 @@
+# Databricks notebook source
+# MAGIC %md ## MLflow Display Tools
+# MAGIC 
+# MAGIC **List Tools**
+# MAGIC * [Count_Objects]($Count_Objects) - summary count of experiments, models and versions
+# MAGIC * [List_Experiments]($List_Experiments)
+# MAGIC * [List_Registered_Models]($List_Registered_Models)
+# MAGIC * [List_Model_Versions]($List_Model_Versions)
+# MAGIC   * [List_Model_Versions_Advanced]($List_Model_Versions_Advanced) - latest versions vs. all versions (besides 'latest' versions)
+# MAGIC 
+# MAGIC **Dump Tools**
+# MAGIC * Coming soon
+# MAGIC 
+# MAGIC **Other**
+# MAGIC * [Common]($Common)
+# MAGIC 
+# MAGIC Last updated: 2023-03-05
