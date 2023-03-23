7389edd89ab0de9e8a9989dbcec0bf4b1208c5cf
amesar <amesar@users.noreply.github.co>
2023-03-23 00:41:40 -0400

    display: finished factoring out common code from display_utils in list scripts

--- mlflow_tools/display/list_model_versions.py
+++ mlflow_tools/display/list_model_versions.py
@@ -4,33 +4,26 @@ List all registered models versions.
 
 import click
 from tabulate import tabulate
+from mlflow_tools.common.click_options import opt_sort_attr, opt_sort_order, opt_columns, opt_output_csv_file
 from mlflow_tools.api import api_factory
-from mlflow_tools.common.click_options import opt_sort_attr, opt_sort_order, opt_output_csv_file
+from mlflow_tools.display.display_utils import process_df
 
 pandas_api = api_factory.get_pandas_api()
 
 
-def to_pandas_dataframe(model_name=None, sort_attr="name", sort_order="asc", use_by_models=False):
+def to_pandas_dataframe(model_name=None, use_by_models=False):
     filter = f"name = '{model_name}'" if model_name else None
     if use_by_models:
         df = pandas_api.search_model_versions_by_models(filter=filter)
     else:
         df = pandas_api.search_model_versions(filter=filter)
-    return _to_pandas_dataframe(df, sort_attr=sort_attr, sort_order=sort_order)
-
-
-def _to_pandas_dataframe(df, sort_attr="name", sort_order="asc"):
-    if sort_attr in df.columns:
-        df.sort_values(by=[sort_attr], inplace=True, ascending=sort_order == "asc")
     return df
 
-
-def list(model_name, csv_file, sort_attr="name", sort_order="asc", use_by_models=False):
-    df = to_pandas_dataframe(model_name, sort_attr, sort_order, use_by_models)
-    if csv_file:
-        with open(csv_file, "w", encoding="utf-8") as f:
-            df.to_csv(f, index=False)
+def list(model_name, columns=None, csv_file=None, sort_attr="name", sort_order="asc", use_by_models=False):
+    df = to_pandas_dataframe(model_name, use_by_models)
+    df = process_df(df, columns, sort_attr, sort_order, csv_file)
     print(tabulate(df, headers="keys", tablefmt="psql", showindex=False))
+    print(f"Versions: {df.shape[0]}")
 
 
 @click.command()
@@ -47,13 +40,20 @@ def list(model_name, csv_file, sort_attr="name", sort_order="asc", use_by_models
     required=False,
     show_default=True
 )
+@opt_columns
 @opt_output_csv_file
 
-def main(model, sort_attr, sort_order, use_by_models, csv_file):
+def main(model, sort_attr, sort_order, use_by_models, columns, csv_file):
     print("Options:")
     for k,v in locals().items(): 
         print(f"  {k}: {v}")
-    list(model, csv_file=csv_file, sort_attr=sort_attr, sort_order=sort_order, use_by_models=use_by_models)
+    list(model, 
+        sort_attr = sort_attr, 
+        sort_order = sort_order,
+        use_by_models = use_by_models,
+        columns = columns, 
+        csv_file = csv_file
+    )
 
 
 if __name__ == "__main__":
--- mlflow_tools/display/list_registered_models.py
+++ mlflow_tools/display/list_registered_models.py
@@ -6,38 +6,35 @@ import click
 from tabulate import tabulate
 from mlflow_tools.api import api_factory
 from mlflow_tools.common.click_options import \
-    opt_sort_attr, opt_sort_order, opt_output_csv_file
+    opt_sort_attr, opt_sort_order, opt_columns, opt_output_csv_file
+from mlflow_tools.display.display_utils import process_df
 
 pandas_api = api_factory.get_pandas_api()
 
 
-def to_pandas_dataframe(sort_attr="name", sort_order="asc"):
-    df = pandas_api.search_registered_models(filter=None)
-    if sort_attr in df.columns:
-        df = df.sort_values(by=[sort_attr], ascending=(sort_order == "asc"))
-    return df
+def to_pandas_dataframe(filter=None):
+    return pandas_api.search_registered_models(filter=filter)
 
 
-def list(csv_file, sort_attr="name", sort_order="asc"):
-    df = to_pandas_dataframe(sort_attr, sort_order)
+def list(sort_attr="name", sort_order="asc", columns=None, csv_file=None):
+    df = to_pandas_dataframe(filter=None)
     print(f"Found {df.shape[0]} models")
+    df = process_df(df, columns, sort_attr, sort_order, csv_file)
     print(tabulate(df, headers="keys", tablefmt="psql", showindex=False))
-    if csv_file:
-        with open(csv_file, "w", encoding="utf-8") as f:
-            df.to_csv(f, index=False)
     print(f"Found {df.shape[0]} models")
 
 
 @click.command()
 @opt_sort_attr
 @opt_sort_order
+@opt_columns
 @opt_output_csv_file
 
-def main(sort_attr, sort_order, csv_file):
+def main(sort_attr, sort_order, columns, csv_file):
     print("Options:")
     for k,v in locals().items():
         print(f"  {k}: {v}")
-    list(csv_file, sort_attr, sort_order)
+    list(sort_attr, sort_order, columns, csv_file)
 
 
 if __name__ == "__main__":
