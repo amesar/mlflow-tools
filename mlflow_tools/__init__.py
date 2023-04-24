c11a3aa76a5e8d36b71191fb705db9be4fe2fafc
amesar <amesar@users.noreply.github.co>
2023-04-24 07:52:55 -0400

    __init__.py: added monkey patch to print tracking URL for MlflowClient as __repr__

--- mlflow_tools/__init__.py
+++ mlflow_tools/__init__.py
@@ -1,1 +1,15 @@
+import json
+import mlflow
 
+# monkey patch mlflow.tracking.MlflowClient to return tracking URI in __repr__
+
+def add_repr_to_MlflowClient():
+    def custom_repr(self):
+        try:
+            return f'{{ "tracking_uri": "{self._tracking_client.tracking_uri}" }}'
+        except AttributeError:
+            return '{{ "tracking_uri": "?" }}'
+    mlflow.client.MlflowClient.__repr__ = custom_repr
+
+
+add_repr_to_MlflowClient()
