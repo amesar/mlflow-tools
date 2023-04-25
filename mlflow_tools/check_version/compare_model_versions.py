22d9524489a7044352c40693ce3646a82784b9ef
amesar <amesar@users.noreply.github.co>
2023-04-25 03:26:01 -0400

    Issue #10: compare_model_versions: flipped --compare options to --skip-compare

--- mlflow_tools/check_version/compare_model_versions.py
+++ mlflow_tools/check_version/compare_model_versions.py
@@ -9,18 +9,18 @@ from mlflow_tools.common import mlflow_utils, io_utils
 from . import local_utils
 from . click_options import opt_download_dir, opt_report_file
 
-def compare_versions(cfg, download_dir, compare_run_models=True, compare_reg_models=True, report_file=None):
-    local_utils.dump(cfg,"Config")
-    if not compare_run_models and not compare_reg_models:
-        print(f"WARNING: Nothing to compare: compare_run_models={compare_run_models} compare_reg_models={compare_reg_models}")
+def compare_versions(config, download_dir, skip_compare_run_models=False, skip_compare_reg_models=False, report_file=None):
+    local_utils.dump(config, "Config")
+    if skip_compare_run_models and skip_compare_reg_models:
+        print(f"WARNING: Nothing to compare: skip_compare_run_models={skip_compare_run_models} skip_compare_reg_models={skip_compare_reg_models}")
         return
 
-    cfg1 = cfg["tracking_server_1"]
+    cfg1 = config["tracking_server_1"]
     client1 = mlflow.MlflowClient(cfg1["host"])
     vr1 = local_utils.get_version(client1, cfg1["model"], cfg1["version_or_stage"])
     vr1.download_uri = client1.get_model_version_download_uri(vr1.name, vr1.version)
 
-    cfg2 = cfg["tracking_server_2"]
+    cfg2 = config["tracking_server_2"]
     client2 = mlflow.MlflowClient(cfg2["host"])
     vr2 = local_utils.get_version(client2, cfg2["model"], cfg2["version_or_stage"])
     vr2.download_uri = client2.get_model_version_download_uri(vr2.name, vr2.version)
@@ -38,17 +38,17 @@ def compare_versions(cfg, download_dir, compare_run_models=True, compare_reg_mod
     local_utils.dump(summary,"Summary")
     report = {
        "System": local_utils.get_system_info(__file__),
-       "Config": cfg,
+       "Config": config,
        "Model_Version_Summary": summary
     }
 
-    if compare_run_models:
+    if not skip_compare_run_models:
         path1 = _download_artifact(client1, vr1.source, cfg1, download_dir, "run_model", 1)
         path2 = _download_artifact(client2, vr2.source, cfg2, download_dir, "run_model", 2)
         run_res = _compare_paths(path1, path2)
         report["Run_Model_Comparison"] = run_res
         local_utils.dump(run_res, "Run_Model_Comparison")
-    if compare_reg_models:
+    if not skip_compare_reg_models:
         path1 = _download_artifact(client1, vr1.download_uri, cfg1, download_dir, "reg_model", 1)
         path2 = _download_artifact(client2, vr2.download_uri, cfg2, download_dir, "reg_model", 2)
         reg_res = _compare_paths(path1, path2)
@@ -56,9 +56,9 @@ def compare_versions(cfg, download_dir, compare_run_models=True, compare_reg_mod
         local_utils.dump(reg_res, "Reg_Model_Comparison")
 
     summary = {}
-    if compare_run_models:
+    if not skip_compare_run_models:
         summary["run_model"] = run_res["equals"]
-    if compare_reg_models:
+    if not skip_compare_reg_models:
         summary["reg_model"] = reg_res["equals"]
     report["Comparison_Summary"] = summary 
     local_utils.dump(summary, "Comparison_Summary")
@@ -101,18 +101,18 @@ def _adjust_path(path, cfg):
     required=True
 )
 @opt_download_dir
-@click.option("--compare-run-models", 
-    help="Compare run models", 
+@click.option("--skip-compare-run-models", 
+    help="Do not compare run models", 
     type=bool, 
     is_flag=True
 )
-@click.option("--compare-reg-models", 
-    help="Compare registered models", 
+@click.option("--skip-compare-reg-models", 
+    help="Do not compare registry models", 
     type=bool, 
     is_flag=True
 )
 @opt_report_file
-def main(config_file, download_dir, compare_run_models, compare_reg_models, report_file):
+def main(config_file, download_dir, skip_compare_run_models, skip_compare_reg_models, report_file):
     """ 
     Compare two model versions: check that the cached registry models are the same and the run models are the same.
     """
@@ -120,7 +120,7 @@ def main(config_file, download_dir, compare_run_models, compare_reg_models, repo
     for k,v in locals().items():
         print(f"  {k}: {v}")
     cfg = io_utils.read_file(config_file)
-    compare_versions(cfg, download_dir, compare_run_models, compare_reg_models, report_file)
+    compare_versions(cfg, download_dir, skip_compare_run_models, skip_compare_reg_models, report_file)
 
     
 if __name__ == "__main__":
