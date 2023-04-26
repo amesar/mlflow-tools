ef129a57008f8c2d279f36c06d4398e49c503778
amesar <amesar>
2023-04-26 07:49:48 +0000

    Issue #10: Updated notebook Model Version Validation README with model version diagram

--- databricks_notebooks/check_version/_README.py
+++ databricks_notebooks/check_version/_README.py
@@ -4,5 +4,11 @@
 # MAGIC **Overview**
 # MAGIC * [Compare_Model_Versions]($Compare_Model_Versions) - Compare two model versions. Check that the cached registry models match and the run models are the same.
 # MAGIC * [Check_Model_Version]($Check_Model_Version) - Checks that a model version's run model artifact matches the cached model registry model.
+# MAGIC * Full details: https://github.com/amesar/mlflow-tools/blob/master/README_check_version.md
 # MAGIC 
-# MAGIC Last updated: 2023-04-25
+# MAGIC **Model Version MLflow Model Diagram**
+# MAGIC <br/><br/>
+# MAGIC <img src="https://github.com/amesar/mlflow-tools/blob/master/docs/MLflow_Model_Version.png?raw=true" width="700" />
+# MAGIC <br/><br/>
+# MAGIC 
+# MAGIC Last updated: 2023-04-26
