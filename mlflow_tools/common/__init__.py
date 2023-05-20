d758c21462914f09e6421ec1283ad5c30f414f83
amesar <amesar@users.noreply.github.co>
2023-05-20 14:01:29 -0400

    common/__init__.py: added http_status_code to MlflowToolsException

--- mlflow_tools/common/__init__.py
+++ mlflow_tools/common/__init__.py
@@ -1,3 +1,5 @@
 class MlflowToolsException(Exception):
-    pass
+    def __init__(self, ex, http_status_code = -1):
+        super().__init__(ex)
+        self.http_status_code = http_status_code
 
