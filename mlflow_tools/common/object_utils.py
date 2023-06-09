92b1919a65d4d7c9c8e97fa3bf3f5cce50061831
amesar <amesar@users.noreply.github.co>
2023-06-08 22:46:24 -0400

    object_utils.py: added scrub_dict and sorted_keys argument

--- mlflow_tools/common/object_utils.py
+++ mlflow_tools/common/object_utils.py
@@ -7,8 +7,17 @@ def obj_to_dict(obj):
     )   
 
 
-def dump_dict_as_json(dct):
-    print(json.dumps(dct, indent=2))
+def dump_dict_as_json(dct, sort_keys=None):
+    print(json.dumps(dct, sort_keys=sort_keys, indent=2))
+
+
+def write_dict_as_json(dct, path, sort_keys=None):
+    with open(path, "w", encoding="utf-8") as f:
+        f.write(json.dumps(dct, sort_keys=sort_keys, indent=2)+"\n")
+
+
+def write_obj_as_json(obj, path, sort_keys=None):
+    write_dict_as_json(obj_to_dict(obj), path, sort_keys)
 
 
 def scrub_dict(obj, bad_key):
