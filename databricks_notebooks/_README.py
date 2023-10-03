a3894a9ce592f299c1f1748f89509beba7a67384
amesar <amesar>
2023-10-03 04:44:03 +0000

    README notebook: Added git repo link

--- databricks_notebooks/_README.py
+++ databricks_notebooks/_README.py
@@ -1,26 +1,29 @@
 # Databricks notebook source
 # MAGIC %md ## mlflow-tools - Databricks Notebooks
 # MAGIC
-# MAGIC **Overview**
+# MAGIC ##### Overview
 # MAGIC * Notebooks to view or manipulate MLflow objects.
 # MAGIC * Install. See [Common]($Common) notebook.
 # MAGIC   * `%pip install git+https:///github.com/amesar/mlflow-tools/#egg=mlflow-tools`
-# MAGIC * Github: https://github.com/amesar/mlflow-tools/tree/master/databricks_notebooks. 
 # MAGIC
-# MAGIC **Notebooks**
+# MAGIC ##### Notebooks
 # MAGIC * [Display notebooks]($display/_README) - view MLflow objects (dump and list), e.g. dump a registered model and its versions in JSON.
 # MAGIC * [Tools notebooks]($tools/_README) - manipulate MLflow objects, e.g. rename a registered model.
 # MAGIC * [Model version validation notebooks]($check_version/_README).
 # MAGIC
-# MAGIC **Console shell script notebook**
+# MAGIC ##### Console shell script notebook
 # MAGIC * Shows how to call command line scripts from a notebook shell (%sh) cell.
 # MAGIC * [Console_Shell_Scripts]($Console_Shell_Scripts) notebook.
 # MAGIC
-# MAGIC **Other**
+# MAGIC ##### Other
 # MAGIC * [Common]($Common)
+# MAGIC
+# MAGIC ##### Git Repo
+# MAGIC * https://github.com/amesar/mlflow-tools
+# MAGIC * https://github.com/amesar/mlflow-tools/tree/master/databricks_notebooks 
 # MAGIC
 # MAGIC **NOTE**
 # MAGIC
 # MAGIC * Requires Databricks Runtime 13 or 12.2. Errors out on 11. :(
 # MAGIC
-# MAGIC Last updated: _2023-07-15_
+# MAGIC Last updated: _2023-10-02_
