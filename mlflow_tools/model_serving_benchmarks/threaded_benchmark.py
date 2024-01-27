00170ff742f06fedda1873ab13986434943d38e0
amesar <amesar@users.noreply.github.co>
2024-01-27 15:17:25 -0500

    Issue #37: got threaded_benchmark.py working

--- mlflow_tools/model_serving_benchmarks/threaded_benchmark.py
+++ mlflow_tools/model_serving_benchmarks/threaded_benchmark.py
@@ -4,7 +4,7 @@ import json
 import threading
 import requests
 import click
-from common import read_data
+from . common import read_data
 
 class MyThread(threading.Thread):
     def __init__(self, args):
@@ -22,11 +22,11 @@ class MyThread(threading.Thread):
 
     def run(self):
         log_filename = f"run_{self.thr_num}.log"
-        with open(log_filename, 'w') as f:
+        with open(log_filename, "w",  encoding="utf-8") as f:
             return self._run(f)
 
     def _run(self,f):
-        headers = { 'Content-Type' : 'application/json' }
+        headers = { "Content-Type" : "application/json" }
         durations = []
         f.write(f"Requests for thread {self.thr_num}:\n")
         num_records = len(self.records)
@@ -36,12 +36,12 @@ class MyThread(threading.Thread):
                 self.num_requests += 1
                 data = json.dumps(r)
                 start = time.time()
-                rsp = requests.post(self.uri, headers=headers, data=data)
+                requests.post(self.uri, headers=headers, data=data, timeout=20)
                 dur = time.time()-start
                 if j % self.log_mod == 0:
-                   f.write(f"  {j}/{num_records}: {round(dur,3)}\n")
-                   ts = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(time.time()))
-                   sys.stdout.write(f" {ts} thr_{self.thr_num:03d} iter:{iter+1}/{self.num_iters} rec:{j}/{num_records}: {round(dur,3)}\n")
+                    f.write(f"  {j}/{num_records}: {round(dur,3)}\n")
+                    ts = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(time.time()))
+                    sys.stdout.write(f" {ts} thr_{self.thr_num:03d} iter:{iter+1}/{self.num_iters} rec:{j}/{num_records}: {round(dur,3)}\n")
                 durations.append(dur)
 
         self.total = sum(durations)
@@ -66,7 +66,7 @@ def fmt(x, prec=3):
     return str(y).ljust(5, '0')
 
 
-def run(uri, data_path, num_records, log_mod, num_threads, num_iters, output_file_base):
+def run(uri, data_path, output_file_base, log_mod, num_records, num_threads, num_iters):
     records = read_data(data_path, num_records)
 
     start_time = time.time()
@@ -86,12 +86,12 @@ def run(uri, data_path, num_records, log_mod, num_threads, num_iters, output_fil
     tot_min = sys.float_info.max
     tot_total = 0
     for i,t in enumerate(threads):
-       _mean,_max,_min, _total, _num_requests = t.get_stats()
-       print(f"  {i:6d} {fmt(_mean)} {fmt(_max)} {fmt(_min)} {fmt(_total,1)} {_num_requests}")
-       tot_total += _total
-       tot_mean += _mean
-       tot_max = max(tot_max,_max)
-       tot_min = min(tot_min,_min)
+        _mean,_max,_min, _total, _num_requests = t.get_stats()
+        print(f"  {i:6d} {fmt(_mean)} {fmt(_max)} {fmt(_min)} {fmt(_total,1)} {_num_requests}")
+        tot_total += _total
+        tot_mean += _mean
+        tot_max = max(tot_max,_max)
+        tot_min = min(tot_min,_min)
     mean = tot_mean / len(threads)
     print(f"  Total  {fmt(mean,4)} {fmt(tot_max)} {fmt(tot_min)}")
 
@@ -121,26 +121,27 @@ def run(uri, data_path, num_records, log_mod, num_threads, num_iters, output_fil
           "duration": duration,
           "mean_thruput": mean_thruput,
         }
-        ts = time.strftime("%Y-%m-%d_%H%M%S", time.gmtime(now))
+        ts = time.strftime("%Y_%m_%d_%H%M%S", time.gmtime(now))
         path = f"{output_file_base}_{ts}.json"
         print("Output file:",path)
-        with open(path, "w") as f:
+        with open(path, "w", encoding="utf-8") as f:
             f.write(json.dumps(dct,indent=2)+"\n")
 
 
 @click.command()
-@click.option("--uri", help="Model serving URI", required=True)
-@click.option("--data-path", help="path for data to score", required=True)
-@click.option("--num-records", help="Num of records", required=True)
-@click.option("--log_mod", help="Log output at this modulo", required=True)
-@click.option("--uri", help="URI", required=True)
-@click.option("--output-file-base", help="Output file base", required=True)
-@click.option("--num-iters", help="Number of iterations over data", required=True)
-def main(uri, data_path, num_records, log_mod, output_file_base, num_iters):
+@click.option("--uri", help="Model serving URI", type=str, required=True)
+@click.option("--data-path", help="path for data to score", type=str, required=True)
+@click.option("--num-records", help="Number of records", type=int, required=True)
+@click.option("--num-threads", help="Number of threads", type=int, required=True)
+@click.option("--num-iters", help="Number of iterations over data", type=int, required=True)
+@click.option("--log-mod", help="Log output at this modulo", type=int, required=True)
+@click.option("--output-file-base", help="Output file base", type=str, required=True)
+
+def main(uri, data_path, output_file_base, log_mod, num_records, num_threads, num_iters):
     print("Options:")
     for k,v in locals().items(): 
         print(f"  {k}: {v}")
-    run(uri, data_path, num_records, log_mod, num_threads, num_iters, output_file_base)
+    run(uri, data_path, output_file_base, log_mod, num_records, num_threads, num_iters)
 
 
 if __name__ == "__main__":
